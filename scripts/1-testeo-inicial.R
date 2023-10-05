87a3a2e69882eb6a2be0404b5c71c1e1cc0602fa
FlamyLeiva <flamy@uchile.cl>
2023-10-05 13:34:56 -0300

    testeo inicial

---
+++ scripts/1-testeo-inicial.R
@@ -0,0 +1,99 @@
+
+pacman::p_load(httr,
+               jsonlite, 
+               mongolite, 
+               tidyverse)
+
+# Descargar descriptionOrchard ------------------------------------------
+mongo(url = 'mongodb+srv://ti-analytics:oS11dxE6qv3T6dYQ@productioncluster.bllew.mongodb.net/', 
+      db = 'db-general',
+      collection = 'DescriptionOrchard') -> DescriptionOrchard
+
+
+
+DescriptionOrchard$find(
+  
+  fields = '{"clientValue": 1, "value": 1, "_id": 1, "location" : 1, "stationId" : 1, "dataSource" : 1}' 
+    
+  ) -> DescriptionOrchard
+    
+
+
+# std names, selección columnas
+DescriptionOrchard |> 
+  unnest_wider(location) |> 
+  
+  rename(orchard = value,
+         id_Analytics = `_id`,
+         client = clientValue) |> 
+  
+  select(client, orchard, 
+         id_Analytics, 
+         lat, lng,
+         stationId, dataSource)  |> 
+  arrange(client, orchard)-> DescriptionOrchard
+
+
+
+
+# Definir huerto 
+
+huerto <- 'Canadilla'
+
+
+GET(paste0('https://api.vilab.cl/index.php/api/clima_pro/',
+           'key/7df5d2f73a99ed699a1955c87050ea7d/',
+           'id/', 
+           DescriptionOrchard |> 
+             filter(orchard == huerto) |> 
+             select(stationId))) -> res
+ 
+res$content |> 
+  rawToChar() |> 
+  fromJSON() |> 
+  pluck(1) |> 
+  as_tibble() |> 
+  
+  rename(datetimePredict = 'Fecha',
+         tempMean = '1',
+         precipitation = '2',
+         relativeHumidityMean = '3',
+         windSpeed = '4') -> forecast
+  
+
+
+# Agregar nuevas columnas 
+
+forecast |> 
+  
+  mutate(orchard = huerto,
+         .before = datetimePredict) |> 
+
+  mutate(datetimeGenerate = Sys.time(), 
+         .before = datetimePredict) |> 
+  
+  mutate(predictIndex = seq(1, nrow(forecast), 1),
+         .after = datetimePredict) -> forecast
+
+
+
+
+# Agregar los datos a mongo 
+mongo(url = 'mongodb+srv://ti-analytics:pO3xLskbi0vJz4nE@prototypecluster.4cmnn9u.mongodb.net/', 
+      db = 'forecastWeather',
+      collection = 'test') -> forecastWeather
+
+
+
+# este punto se debe mejorar, considerando ir actualizando la data
+
+forecastWeather$insert(forecast)
+
+
+
+
+
+
+
+
+
